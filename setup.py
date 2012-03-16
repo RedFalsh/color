ef8244cf9836fe5001bf36550fbfa77a3ad916fa
Tres Walsh <tres.walsh@mnmlstc.com>
2012-03-16 14:14:49 -0700

    Added License to MANIFEST, Added Readme.rst as long_description.

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ config.read(os.path.join(os.getcwd(), 'setup.cfg'))
 metadata = config['metadata']
 packages = config['files']
 
+readme = os.path.join(os.path.dirname(__file__), metadata['description-file'])
+
 setup(
     name=metadata['name'],
     version=metadata['version'],
@@ -18,6 +20,7 @@ setup(
     author=metadata['author'],
     url=metadata['home-page'],
     description=metadata['summary'],
+    long_description=open(readme).read(),
     keywords=metadata['keywords'],
     classifiers=metadata['classifiers'].split('\n')[1:],
     install_requires=['distribute'],
